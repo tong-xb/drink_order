6fd06c72e22926609f12ddccc21329a76cbe95d6
Lyanna <Lyanna.Chen@Yuanta.com>
2023-04-21 17:14:23 +0800

    modify bank-fido string to order-system

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,6 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
-    baseUrl: 'http://localhost:4173/bank-fido/',
+    baseUrl: 'http://localhost:4173/order-system/',
   },
 });
