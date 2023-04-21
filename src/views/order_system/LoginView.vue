cae5b77bc7cad9f18888ebba0f24874e9eabdf5c
Lyanna <Lyanna.Chen@Yuanta.com>
2023-04-21 17:22:33 +0800

    add store check login mechanism

--- src/views/order_system/LoginView.vue
+++ src/views/order_system/LoginView.vue
@@ -1,27 +1,29 @@
 <template>
-  <h1>訂餐系統</h1>
   <div>
-    <div class="container">
-      <h2>登入</h2>
-      <div>
-        <label for="useraccount">
-          <b>User Account</b>
-        </label>
-        <input type="text" placeholder="Enter User Account" name="useraccount" v-model="user.userAccount" />
+    <h1>訂餐系統</h1>
+    <div>
+      <div class="container">
+        <h2>登入</h2>
+        <div>
+          <label for="useraccount">
+            <b>User Account</b>
+          </label>
+          <input type="text" placeholder="Enter User Account" name="useraccount" v-model="user.userAccount" />
+        </div>
+        <div>
+          <label for="useraccount">
+            <b>User Password</b>
+          </label>
+          <input
+            type="text"
+            placeholder="Enter User Password"
+            name="userpassword"
+            v-model="user.userPassword"
+            @keyup.enter="login"
+          />
+        </div>
+        <button @click="login">Login</button>
       </div>
-      <div>
-        <label for="useraccount">
-          <b>User Password</b>
-        </label>
-        <input
-          type="text"
-          placeholder="Enter User Password"
-          name="userpassword"
-          v-model="user.userPassword"
-          @keyup.enter="login"
-        />
-      </div>
-      <button @click="login">Login</button>
     </div>
   </div>
 </template>
@@ -29,32 +31,49 @@
 <script setup>
 import { reactive } from 'vue';
 // import users from '@/api/json/users.json';
+import { useRouter } from 'vue-router';
+import { useLoginStore } from '@/stores/loginState';
+
+const router = useRouter();
+const store = useLoginStore();
+
+const toHome = () => {
+  router.push({ name: 'HomeView' });
+};
+
 const user = reactive({
   userAccount: '',
   userPassword: '',
+});
+
+const localStorageUser = reactive({
+  id: '',
   userName: 'unknown',
   isAdmin: '',
 });
 const usersList = reactive({
   users: [
-    { account: 'user1@yuanta.com', accountPassword: '0000', userName: '張明宏', isAdmin: false },
-    { account: 'user2@yuanta.com', accountPassword: '0000', userName: '張雅婷', isAdmin: false },
-    { account: 'user3@yuanta.com', accountPassword: '0000', userName: '陳家豪', isAdmin: false },
-    { account: 'user4@yuanta.com', accountPassword: '0000', userName: '林志明', isAdmin: false },
-    { account: 'admin1@yuanta.com', accountPassword: '0000', userName: '張明宏', isAdmin: true },
+    { id: '001', account: 'user1@yuanta.com', accountPassword: '0000', userName: '張明宏', isAdmin: false },
+    { id: '002', account: 'user2@yuanta.com', accountPassword: '0000', userName: '張雅婷', isAdmin: false },
+    { id: '003', account: 'user3@yuanta.com', accountPassword: '0000', userName: '陳家豪', isAdmin: false },
+    { id: '004', account: 'user4@yuanta.com', accountPassword: '0000', userName: '林志明', isAdmin: false },
+    { id: '005', account: 'admin1@yuanta.com', accountPassword: '0000', userName: '張明宏', isAdmin: true },
   ],
 });
 const login = () => {
   usersList.users.forEach((usersListItem) => {
     if (usersListItem.account === user.userAccount) {
       if (usersListItem.accountPassword === user.userPassword) {
-        user.userName = usersListItem.userName;
-        user.isAdmin = usersListItem.isAdmin;
-        localStorage.setItem('user', JSON.stringify(user));
+        localStorageUser.userName = usersListItem.userName;
+        localStorageUser.isAdmin = usersListItem.isAdmin;
+        localStorageUser.id = usersListItem.id;
+        store.isLogin = true;
+        localStorage.setItem('user', JSON.stringify(localStorageUser));
+        toHome();
       }
     }
   });
-  console.log('userName:' + user.userName + ', isAdmin:' + user.isAdmin);
+  console.log('userName:' + localStorageUser.userName + ', isAdmin:' + localStorageUser.isAdmin);
 };
 </script>
 
