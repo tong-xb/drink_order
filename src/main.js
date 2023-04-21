17f71a2238010cfe8d0ddc50e7ee0661d1c808f6
Lyanna <Lyanna.Chen@Yuanta.com>
2023-04-21 17:16:57 +0800

    1. modify App from App.vue to OrderApp.vue 2. mark i18n

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 
-import App from './App.vue';
+// import App from './App.vue';
+import App from './OrderApp.vue';
 import router from './router';
-import i18n from './i18n.js';
+// import i18n from './i18n.js';
 
 import './assets/css/main.css';
 
 const app = createApp(App);
 
-app.use(i18n);
+// app.use(i18n);
 app.use(createPinia());
 app.use(router);
 
