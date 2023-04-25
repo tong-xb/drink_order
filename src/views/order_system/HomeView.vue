1c618c4d0628324106608a1496fb9f75e6c6a91c
Lyanna <Lyanna.Chen@Yuanta.com>
2023-04-25 13:34:31 +0800

    modify click event

--- src/views/order_system/HomeView.vue
+++ src/views/order_system/HomeView.vue
@@ -1,25 +1,26 @@
 <template>
   <div v-for="menuItem in menus.menu" :key="menuItem.key">
     <MenuCard
-      :id="menuItem.id"
+      :id="menuItem.menuId"
       :storeName="menuItem.storeName"
       :storeType="menuItem.storeType"
       :openTimeFrom="menuItem.openTimeFrom"
       :openTimeTo="menuItem.openTimeTo"
       :arrivalTime="menuItem.arrivalTime"
+      @click.prevent.stop="clickTest(menuItem.menuId)"
     />
   </div>
 </template>
 <script setup>
 import MenuCard from '@/components/order_system/card/MenuCard.vue';
 import { reactive } from 'vue';
-// import users from '@/api/json/users.json';
-// import { useRouter } from 'vue-router';
+import { useRouter } from 'vue-router';
 
 const menus = reactive({
   menu: [
     {
-      id: '001',
+      menuId: 'm001',
+      storeId: 's001',
       storeName: '50嵐',
       storeType: '飲料',
       openTimeFrom: '4/19 (三) 10:00',
@@ -27,38 +28,20 @@ const menus = reactive({
       arrivalTime: '4/19 (三) 12:00',
     },
     {
-      id: '002',
+      menuId: 'm002',
+      storeId: 's001',
       storeName: '龜記',
       storeType: '飲料',
       openTimeFrom: '4/20 (四) 10:00',
       openTimeTo: '4/20 (四) 11:00',
       arrivalTime: '4/20 (四) 12:00',
     },
-    {
-      id: '003',
-      storeName: '麥當勞',
-      storeType: '主食',
-      openTimeFrom: '4/19 (三) 10:00',
-      openTimeTo: '4/19 (三) 11:00',
-      arrivalTime: '4/19 (三) 12:00',
-    },
   ],
 });
 
-// const router = useRouter();
-
-// const toLogin = () => {
-//   router.push({ name: 'LoginView' });
-// };
-
-// const localStorageUser = JSON.parse(localStorage.getItem('user'));
-// console.log(localStorageUser);
-
-// onBeforeMount(() => {
-//   if (!localStorageUser) {
-//     // alert('請先登入!');
-//     router.push({ name: 'LoginView' });
-//     toLogin();
-//   }
-// });
+const router = useRouter();
+const clickTest = (mId) => {
+  console.log(mId);
+  router.push({ name: 'MenuView', params: { menuId: mId } });
+};
 </script>
