e72961a3dea2d39a40e4536ae8601d6e0286b03d
Lyanna <Lyanna.Chen@Yuanta.com>
2023-04-27 17:07:56 +0800

    move repeat code to MyOrderCard.vue

--- src/views/order_system/MyOrderView.vue
+++ src/views/order_system/MyOrderView.vue
@@ -5,35 +5,29 @@
         全部
       </select>
     </div>
-
-    <div class="order_container">
-      <div class="order_container_title">
-        <h1>50嵐</h1>
-        <p>4/19 (三) 13:30</p>
-        <a>></a>
-      </div>
-      <p>預計送達時間: 4/19 (三) 13:30</p>
-      <div class="order_container_content">
-        <div class="left"><a>1</a></div>
-        <div class="middle">
-          <p class="name">茉莉綠茶</p>
-          <p class="customize">微糖 微冰</p>
-        </div>
-        <div class="right">
-          <p>$30</p>
-        </div>
-      </div>
-      <div class="order_container_footer">
-        <div class="total_container">
-          <div class="total">$30</div>
-        </div>
-        <div class="button_container">
-          <button>刪除</button>
-        </div>
-      </div>
+    <div class="card_container" v-for="cart in sortedCart" :key="cart">
+      <MyOrderCard :menuId="cart" />
     </div>
   </div>
 </template>
+
+<script setup>
+import MyOrderCard from '@/components/order_system/card/MyOrderCard.vue';
+import { computed } from 'vue';
+import { useCart } from '@/stores/order_system/cart';
+
+const storeCart = useCart();
+
+const sortedCart = computed(() => {
+  // menuIdList: [m001,m001,m001,002]
+  let menuIdList = storeCart.cart.all.map((e) => e.menuId);
+  // myMenu: [m001,002]
+  let myMenu = menuIdList.filter((el, idx, arr) => {
+    return arr.indexOf(el) === idx;
+  });
+  return myMenu;
+});
+</script>
 
 <style>
 .select_container {
@@ -41,66 +35,12 @@
 }
 .myOrder_container {
   padding: 20px;
-  max-width: 450px;
 }
 .myOrder_container select {
   margin-bottom: 20px;
   float: right;
 }
-.order_container {
-  padding: 10px 20px;
-  border: #939393 solid 2px;
-  border-radius: 15px;
-}
-.order_container h1 {
-  margin: 15px 0;
-}
-.order_container_title {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-.order_container p {
-  padding: 0;
-}
-.order_container_content {
-  display: flex;
-  padding: 20px 0 10px 0;
-}
-.order_container_content .left {
-  width: 5%;
-}
-.order_container_content .left a {
-  background-color: #d3d3d3;
-  padding: 2px;
-}
-.order_container_content .middle {
-  width: 80%;
-  display: flex;
-  flex-direction: column;
-  padding: 0;
-}
-.order_container_content .right {
-  width: 15%;
-}
-.name {
-  font-size: larger;
-  font-weight: bold;
-}
-.customize {
-  color: #939393;
-}
-.order_container_footer .total {
-  width: 15%;
-  float: right;
-}
-.total_container {
-  border-top: #939393 solid 1px;
-  padding-top: 10px;
-  height: 50px;
-}
-.button_container {
-  display: flex;
-  justify-content: center;
+.card_container {
+  padding-bottom: 10px;
 }
 </style>
