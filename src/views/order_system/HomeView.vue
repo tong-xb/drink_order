182f98513f61e3273d18c759a5a64da199967c34
Lyanna <Lyanna.Chen@Yuanta.com>
2023-05-02 17:14:04 +0800

    add admin function

--- src/views/order_system/HomeView.vue
+++ src/views/order_system/HomeView.vue
@@ -1,15 +1,20 @@
 <template>
-  <div v-for="menuItem in menus.menu" :key="menuItem.key">
-    <MenuCard
-      :id="menuItem.menuId"
-      :storeName="menuItem.storeName"
-      :storeType="menuItem.storeType"
-      :openTimeFrom="menuItem.openTimeFrom"
-      :openTimeTo="menuItem.openTimeTo"
-      :arrivalTime="menuItem.arrivalTime"
-      @click.prevent.stop="clickTest(menuItem.menuId)"
-    />
-  </div>
+  <main>
+    <div class="card-container">
+      <MenuCard :storeName="'開放訂餐'" @click.prevent.stop="openOrder()" />
+    </div>
+    <div class="card-container" v-for="menuItem in menus.menu" :key="menuItem.key">
+      <MenuCard
+        :id="menuItem.menuId"
+        :storeName="menuItem.storeName"
+        :storeType="menuItem.storeType"
+        :openTimeFrom="menuItem.openTimeFrom"
+        :openTimeTo="menuItem.openTimeTo"
+        :arrivalTime="menuItem.arrivalTime"
+        @click.prevent.stop="openMenu(menuItem.menuId)"
+      />
+    </div>
+  </main>
 </template>
 <script setup>
 import MenuCard from '@/components/order_system/card/MenuCard.vue';
@@ -40,8 +45,21 @@ const menus = reactive({
 });
 
 const router = useRouter();
-const clickTest = (mId) => {
-  console.log(mId);
+
+const openOrder = () => {
+  router.push({ name: 'OpenOrderView' });
+};
+const openMenu = (mId) => {
+  // console.log(mId);
   router.push({ name: 'MenuView', params: { menuId: mId } });
 };
 </script>
+
+<style lang="scss" scoped>
+main {
+  display: flex;
+  justify-content: flex-start;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
