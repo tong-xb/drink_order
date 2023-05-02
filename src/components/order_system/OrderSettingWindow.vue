cd4dead198c00e8178827045eb01d88a89393446
Lyanna <Lyanna.Chen@Yuanta.com>
2023-05-02 17:11:14 +0800

    add new file

---
+++ src/components/order_system/OrderSettingWindow.vue
@@ -0,0 +1,106 @@
+<template>
+  <div>
+    <div class="modal-background"></div>
+    <div class="modal-base">
+      <!-- Modal content -->
+      <div class="modal-content">
+        <div class="orderHeader">
+          <h1>50嵐</h1>
+        </div>
+        <div class="orderBody">
+          <div class="item">
+            <h2>訂購時間:</h2>
+            <input type="text" />
+          </div>
+          <div class="item">
+            <h2>預計到達時間:</h2>
+            <input type="text" />
+          </div>
+          <div class="item">
+            <h2>備註:</h2>
+            <input type="text" />
+          </div>
+        </div>
+        <div class="orderFooter">
+          <button @click="addToCart">加入訂單</button>
+          <button @click="closeWindow">取消</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const emit = defineEmits('emitWindowOpen');
+const closeWindow = () => {
+  emit('emitWindowOpen', false);
+};
+</script>
+
+<style lang="scss" scoped>
+.modal-background {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 99;
+  background-color: rgba(0, 0, 0, 0.4);
+  transition: opacity 0.3s ease-in-out;
+}
+.modal-base {
+  position: fixed; /* Stay in place */
+  z-index: 100; /* Sit on top */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: hidden; /* Enable scroll if needed */
+}
+.modal-content {
+  display: flex;
+  flex-direction: column;
+  background-color: #fefefe;
+  margin: 10vh 5vh;
+  padding: 10px 20px;
+  border: 1px solid #888;
+  border-radius: 0.5rem;
+  height: 80vh;
+  font-size: 1.1rem;
+}
+.orderHeader {
+  height: 15%;
+  text-align: center;
+}
+.orderBody {
+  overflow: auto;
+  height: 70%;
+}
+.orderBody-content {
+  padding: 5px 0;
+}
+.orderBody-content h3 {
+  padding: 5px 0;
+}
+.radioSelector_container {
+  display: flex;
+  justify-content: space-between;
+}
+.radioSelector input {
+  margin: 0 5px 0 0;
+}
+.radioSelector div {
+  padding-top: 5px;
+}
+.orderFooter {
+  height: 15%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+.orderFooter button {
+  width: 80%;
+  margin: 3px;
+}
+</style>
--- src/views/order_system/MyAccountView.vue
+++ src/views/order_system/MyAccountView.vue
@@ -1,3 +1,94 @@
 <template>
-  <h1>MyAccount</h1>
+  <div ref="infoCard" class="info-card">
+    <div class="card-header">
+      <div class="item-first">
+        <div>個人資料</div>
+      </div>
+    </div>
+    <div class="card-body">
+      <div class="item">
+        <div>姓名</div>
+        <div>張雅婷</div>
+      </div>
+      <div class="item">
+        <div>身分</div>
+        <div>一般使用者</div>
+      </div>
+      <div class="item">
+        <div>帳號</div>
+        <div>user1@yuanta.com</div>
+      </div>
+      <div class="item">
+        <div>密碼</div>
+        <div>0000</div>
+      </div>
+    </div>
+    <div class="card-footer"><button @click="logout">登出</button></div>
+  </div>
 </template>
+
+<script setup>
+// import { useRouter } from 'vue-router';
+import { useLoginStore } from '@/stores/order_system/loginState';
+
+const userStore = useLoginStore();
+// const router = useRouter();
+
+const logout = () => {
+  if (confirm('確定要登出嗎?') == true) {
+    userStore.isLogin = false;
+    userStore.user.user = { userId: '', userName: '', isAdmin: false };
+    localStorage.removeItem('user');
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.info-card {
+  box-shadow: 0px 2px 4px #c5c5c5;
+  margin: 15px;
+  border-radius: 10px;
+  font-size: 1em;
+  line-height: 1.425em;
+
+  .card-header {
+    padding-top: 15px;
+    padding-bottom: 8px;
+    margin-left: 15px;
+    margin-right: 15px;
+    border-bottom: #959595 solid 1px;
+    font-size: 1.1em;
+    align-items: center;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .card-item {
+      padding-top: 8px;
+      div {
+        white-space: nowrap;
+      }
+    }
+  }
+
+  .card-body {
+    padding-bottom: 15px;
+    margin-left: 15px;
+    .item {
+      padding-top: 8px;
+      padding-right: 15px;
+      div {
+        white-space: nowrap;
+      }
+    }
+  }
+  .card-footer {
+    padding-top: 8px;
+    padding-bottom: 15px;
+    margin-left: 15px;
+    margin-right: 15px;
+    border-top: #959595 solid 1px;
+    display: flex;
+    justify-content: center;
+  }
+}
+</style>
