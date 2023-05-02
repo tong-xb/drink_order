b046b5634cba7cac4d0b73335bddfb3859aefdb8
Lyanna <Lyanna.Chen@Yuanta.com>
2023-05-02 17:13:15 +0800

    add count cart function

--- src/components/order_system/footer/BasicFooter.vue
+++ src/components/order_system/footer/BasicFooter.vue
@@ -1,10 +1,23 @@
 <template>
   <div class="footer">
     <router-link :to="{ name: 'HomeView' }">首頁</router-link>
-    <router-link :to="{ name: 'MyOrderView' }">訂單</router-link>
+    <router-link :to="{ name: 'MyOrderView' }"
+      >訂單
+      <a v-if="count != 0">{{ count }}</a>
+    </router-link>
     <router-link :to="{ name: 'MyAccountView' }">帳戶</router-link>
   </div>
 </template>
+
+<script setup>
+import { useCart } from '@/stores/order_system/cart';
+import { computed } from 'vue';
+const storeCart = useCart();
+
+const count = computed(() => {
+  return storeCart.cart.unsentCart.length;
+});
+</script>
 
 <style>
 .footer {
