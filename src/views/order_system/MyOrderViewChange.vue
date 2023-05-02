3f40634a8ef752b05e404eefa7f6efb7622f21dd
Lyanna <Lyanna.Chen@Yuanta.com>
2023-05-02 17:14:57 +0800

    complete show cart function

--- src/views/order_system/MyOrderViewChange.vue
+++ src/views/order_system/MyOrderViewChange.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <div>
-      <h1>change</h1>
-    </div>
-    <div class="card_container" v-for="cart in storeCart.sentCart" :key="cart.menuId">
+    <div class="card_container" v-for="cart in storeCart.cart.sentCart" :key="cart.menuId">
       <MyOrderCard :menuId="cart.menuId" :cartType="'sentCart'" />
+    </div>
+    <div class="empty" v-if="isEmpty">
+      <h1>無未完成訂單!</h1>
     </div>
   </div>
 </template>
@@ -12,5 +12,19 @@
 <script setup>
 import MyOrderCard from '@/components/order_system/card/MyOrderCard.vue';
 import { useCart } from '@/stores/order_system/cart';
+import { computed } from 'vue';
 const storeCart = useCart();
+
+const isEmpty = computed(() => {
+  if (storeCart.cart.sentCart.length === 0) {
+    return true;
+  }
+  return false;
+});
 </script>
+
+<style lang="scss" scoped>
+.empty {
+  color: antiquewhite;
+}
+</style>
--- src/views/order_system/MyOrderViewComplete.vue
+++ src/views/order_system/MyOrderViewComplete.vue
@@ -1,5 +1,30 @@
 <template>
   <div>
-    <h1>complete</h1>
+    <div class="card_container" v-for="cart in storeCart.cart.completeCart" :key="cart.menuId">
+      <MyOrderCard :menuId="cart.menuId" :cartType="'completeCart'" />
+    </div>
+    <div class="empty" v-if="isEmpty">
+      <h1>無歷史訂單!</h1>
+    </div>
   </div>
 </template>
+
+<script setup>
+import MyOrderCard from '@/components/order_system/card/MyOrderCard.vue';
+import { useCart } from '@/stores/order_system/cart';
+import { computed } from 'vue';
+const storeCart = useCart();
+
+const isEmpty = computed(() => {
+  if (storeCart.cart.completeCart.length === 0) {
+    return true;
+  }
+  return false;
+});
+</script>
+
+<style lang="scss" scoped>
+.empty {
+  color: antiquewhite;
+}
+</style>
